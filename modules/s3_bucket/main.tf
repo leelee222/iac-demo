0e6933def6c53f3cfa5a15d700d5891fdc19ade8
Ilham <ilham.adios@epitech.eu>
2025-11-12 08:35:51 +0100

    correcting security improvements and removing unnecessary checks

--- modules/s3_bucket/main.tf
+++ modules/s3_bucket/main.tf
@@ -7,6 +7,23 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_versioning" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "random_id" "bucket_id" {
   byte_length = 4
 }
